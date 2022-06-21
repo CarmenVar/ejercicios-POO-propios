3693e1e3405f92b31669666eb9b13cf9b8886be2
Carmen Vargas <trishiavargas@gmail.com>
2022-06-21 00:52:23 -0300

    Mi primer programa POO

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner mascotitas = new Scanner(System.in);
+        String nombre = "Manchitas";
+        int edad = 2;
+        double cantidadDeComida = 1.5;
+        String sonidoQueHace = "guau guau";
+        System.out.println(nombre + " tiene " + edad +  " años");
+        System.out.println(nombre + " come "+ cantidadDeComida + " kgs. y "+ " hace "+ sonidoQueHace);
+
+        String nombre1 = "Nemo";
+        int edad1 = 1;
+        int cantidadDeComida1 = 140;
+        String sonidoQueHace1 = "blirp blirp";
+        System.out.println(nombre1 + " tiene " + edad1 +  " años");
+        System.out.println(nombre1 + " come "+ cantidadDeComida1 + " grs. y "+ " hace "+ sonidoQueHace1);
+
+        String nombre2 = "Silvestre";
+        int edad2 = 3;
+        int cantidadDeComida2 = 500;
+        String sonidoQueHace2 = "miau miau";
+        System.out.println(nombre2 + " tiene " + edad2 +  " años");
+        System.out.println(nombre2 + " come "+ cantidadDeComida2 + " grs. y "+ " hace "+ sonidoQueHace2);
+
+        String nombre3 = "Manuelita";
+        int edad3 = 1;
+        int cantidadDeComida3 = 300;
+        String sonidoQueHace3 = "turp turp";
+        System.out.println(nombre3 + " tiene " + edad3 +  " años");
+        System.out.println(nombre3 + " come "+ cantidadDeComida3 + " grs. y "+ " hace "+ sonidoQueHace3);
+
+        String nombre4 = "Gardel";
+        int edad4 = 1;
+        int cantidadDeComida4 = 350;
+        String sonidoQueHace4 = "pio pio";
+        System.out.println(nombre4 + " tiene " + edad4 +  " años");
+        System.out.println(nombre4 + " come "+ cantidadDeComida4 + " grs. y "+ " hace "+ sonidoQueHace4);
+    }
+}
